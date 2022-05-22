fa4bcccc0e11a7f62b21d60647e5c8907ce515af
gabbjuncos <gabbjuncos@gmail.com>
2022-05-22 01:00:07 -0300

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { RouterModule } from '@angular/router';
-import { APP_BASE_HREF} from '@angular/common';  
-import { ReactiveFormsModule } from "@angular/forms";
+import { APP_BASE_HREF } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { MyInterceptor } from "./shared/my-interceptor";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MyInterceptor } from './shared/my-interceptor';
 
 import {
   NgbPaginationModule,
-  NgbModalModule
-} from "@ng-bootstrap/ng-bootstrap";
+  NgbModalModule,
+} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from "./app.component";
-import { MenuComponent } from "./components/menu/menu.component";
-import { InicioComponent } from "./components/inicio/inicio.component";
-import { ArticulosComponent } from "./components/articulos/articulos.component";
-import { ArticulosFamiliasComponent } from "./components/articulos-familias/articulos-familias.component";
-import { ModalDialogComponent } from "./components/modal-dialog/modal-dialog.component";
+import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
+import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
+import { ModalDialogComponent } from './components/modal-dialog/modal-dialog.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { ModalDialogComponent } from "./components/modal-dialog/modal-dialog.com
     ArticulosComponent,
     ArticulosFamiliasComponent,
     ModalDialogComponent,
+    ClientesComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,16 +39,17 @@ import { ModalDialogComponent } from "./components/modal-dialog/modal-dialog.com
       { path: '', redirectTo: '/inicio', pathMatch: 'full' },
       { path: 'inicio', component: InicioComponent },
       { path: 'articulos', component: ArticulosComponent },
-      { path: 'articulosfamilias', component: ArticulosFamiliasComponent }
+      { path: 'clientes', component: ClientesComponent },
+      { path: 'articulosfamilias', component: ArticulosFamiliasComponent },
     ]),
     NgbPaginationModule,
     NgbModalModule,
   ],
   entryComponents: [ModalDialogComponent],
   providers: [
-     {provide: APP_BASE_HREF, useValue : '/' },
-    { provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi: true }
+    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
